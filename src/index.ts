929d3250a9733c6bbee67dfc0bd36bdff466e5b3
pierreviences <imaniyazid@gmail.com>
2023-08-21 18:54:31 +0700

    feat: Finished project

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 const app = express();
 
 import authorRouter from "./routes/author.router";
+import bookRouter from "./routes/book.router";
 
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 app.get("/ping", (req: express.Request, res: express.Response) => {
   res

3728ba1dda25362cd5ba476af9c6e4766207cf8b
pierreviences <imaniyazid@gmail.com>
2023-08-21 18:29:48 +0700

    feat: Create Controller

--- src/controllers/author.controllers.ts
+++ src/controllers/author.controllers.ts
@@ -43,6 +43,21 @@ export const getAuthorById = async (
 };
 
 // create Author
+export const createAuthor = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const authorData = req.body;
+    const author = await authorClient.create({
+      data: authorData,
+    });
+
+    res.status(200).json({ data: author });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // update Author
 
